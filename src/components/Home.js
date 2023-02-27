21fee0c7639a67578d9d37829e09be9c3c303a5a
UlaşDK <udk97@hotmail.com>
2023-02-27 13:50:34 +0300

    navbar responsive

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,30 +9,39 @@ function Home() {
   return (
     <div>
       <Navbar />
-        {/* <div className="flex flex-row w-1/2 h-screen bg-sky-300 justify-center"></div> */}
-        <div className="rounded w-max h-max  mt-20 flex flex-row">
-          <div className="bg-sky-100  ">
-            <img
-              className="w-60 h-60 rounded-full m-10 shadow-2xl"
-              src="https://marmotamaps.com/de/fx/wallpaper/download/faszinationen/Marmotamaps_Wallpaper_Berchtesgaden_Desktop_1920x1080.jpg"
-            ></img>
-            <h2 className="shortCardText mt-5">{"Ulaş Doruk Karaman"}</h2>
-            <h3 className="shortCardText  mb-5">{"Jr.Web Developer"}</h3>
-            <footer className="bg-white">
-              <div className="p-2 flex justify-around">
-                <FiGithub className="w-7 h-7" />
-                <FaLinkedinIn className="w-7 h-7" />
-                <FiInstagram className="w-7 h-7" />
-              </div>
-            </footer>
+      <div className="flex justify-center">
+        <div className="flex justify-center  lg:flex-wrap ">
+          <div className="rounded  h-max w-max mt-36 md:flex md:justify-center ">
+            <div className="bg-sky-100 ">
+              <img
+                className="sm:w-60 sm:h-60 w-36 h-36 rounded-full m-10 shadow-2xl "
+                src="https://marmotamaps.com/de/fx/wallpaper/download/faszinationen/Marmotamaps_Wallpaper_Berchtesgaden_Desktop_1920x1080.jpg"
+              ></img>
+              <h2 className="shortCardText mt-5">Ulaş Doruk Karaman</h2>
+              <h3 className="shortCardText  mb-5">Jr.Web Developer</h3>
+              <footer className="bg-white">
+                <div className="p-2 flex justify-around ">
+                  <FiGithub className="w-7 h-7" />
+                  <FaLinkedinIn className="w-7 h-7" />
+                  <FiInstagram className="w-7 h-7" />
+                </div>
+              </footer>
+            </div>
+            <div className="bg-white ">
+              <h1 className="font-bold font-sans text-7xl ml-20 mr-20 mt-5">
+                Hello
+              </h1>
+              <h2 className="font-bold font-sans mt-5 sm:text-2xl">
+                Here's who I am & what I do
+              </h2>
+              <span className="font-bold w-64 m-10 flex justify-around text-justify">
+                I graduated from the Hacettepe University Mining Engineering
+                department in August 2021. I worked for eight months as an
+                Underground Mining Engineer. Besides, ı am trying to improve
+                myself in the field of Web Development on my own.
+              </span>
+            </div>
           </div>
-          <div className="bg-white ">
-            <h1 className="font-bold font-sans text-7xl ml-20 mr-20 mt-5">
-              {"Hello"}
-            </h1>
-            <span className="font-bold w-64 m-10 flex justify-around text-justify">
-              {` I graduated from the Hacettepe University Mining Engineering department in August 2021. I worked for eight months as an Underground Mining Engineer. Besides, ı am trying to improve myself in the field of Web Development on my own. `}
-            </span>
         </div>
       </div>
     </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,14 +3,14 @@ import { IoPersonSharp } from "react-icons/io5";
 
 function Navbar() {
   return (
-    <div className="bg-amber-100 p-4 flex justify-between  ">
-      <Link to="/" className="flex justify-center m-2">
+    <div className="bg-amber-100 p-4  justify-center lg:justify-between flex stickt top-0 ">
+      <Link to="/" className=" justify-center m-2 hidden lg:flex">
         <IoPersonSharp className="w-10 h-10" />
         <h1 className="font-bold text-2xl font-mono ml-6 mt-1 ">
           {"Ulaş Doruk Karaman / Jr.Web Developer"}
         </h1>
       </Link>
-      <div className="flex justify-around mr-10 ">
+      <div className="flex justify-around md:mr-10 ">
         <Link to="/" className="m-2">
           <button className="btn">About Me</button>
         </Link>
